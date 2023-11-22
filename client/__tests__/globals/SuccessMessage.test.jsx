cbf8cf0752b35c4db35a873e7515b6ffb1c76e39
williamdev <william-ca@live.no>
2023-11-22 19:43:08 +0100

    tests for globals

--- client/__tests__/globals/SuccessMessage.test.jsx
+++ client/__tests__/globals/SuccessMessage.test.jsx
@@ -1,16 +1,16 @@
 import renderer, {act} from "react-test-renderer";
 import {MemoryRouter} from "react-router-dom";
-import ErrorMessage from "../../components/globals/ErrorMessage.jsx";
+import SuccessMessage from "../../components/globals/SuccessMessage.jsx";
 
 describe("error message", () => {
 
-    it("show a error message", () => {
+    it("show a success message!", () => {
 
-        const message = "This is an error!";
+        const message = "This is a success message!";
 
         const component = renderer.create(
             <MemoryRouter>
-                <ErrorMessage message={message}/>
+                <SuccessMessage message={message}/>
             </MemoryRouter>);
         expect(component).toMatchSnapshot();
     })
